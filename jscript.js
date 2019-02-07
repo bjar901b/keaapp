d58dc040a7bce928632e2fd255d1f13dedc95b7d
bjar901b <47105008+bjar901b@users.noreply.github.com>
2019-02-07 10:04:14 +0100

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -176,7 +176,9 @@ function incHeight() {
 
 function decHeight() {
 	sit--;
+	var minusData = stringToBytes(sit);
 	document.getElementById("newHeight").innerHTML = sit;
+	ble.writeWithoutResponse(ConnDeviceId, blue.serviceUUID, blue.txCharacteristic, minusData, onSend, onError);
 }
 
 function sendStand() {
