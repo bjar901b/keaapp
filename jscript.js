90b8be1e7a69721427bd259c6b9e2a5c8defd31b
bjar901b <47105008+bjar901b@users.noreply.github.com>
2019-02-07 10:30:33 +0100

    2 ting

--- jscript.js
+++ jscript.js
@@ -96,7 +96,7 @@ function refreshDeviceList(){
 
 function onDiscoverDevice(device){
 	//Make a list in html and show devises
-		if(device.name == "*Insert name*") {
+		if(device.name == "*Bord 4*") {
 		var listItem = document.createElement('li'),
 		html = device.name+ "," + device.id;
 		listItem.innerHTML = html;
